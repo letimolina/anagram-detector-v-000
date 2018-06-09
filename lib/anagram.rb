714c64c055e0527fb8857a936a9363651b7fea77
Leticia Molina <letimolina@gmail.com>
2018-06-09 22:28:22 +0000

    automatically backed up by learn

---
+++ lib/anagram.rb
@@ -0,0 +1,18 @@
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    matched = []
+    words.each do |word|
+      if word.split("").sort == self.word.split("").sort
+        matched << word
+      end
+    end
+    matched
+  end
+end
